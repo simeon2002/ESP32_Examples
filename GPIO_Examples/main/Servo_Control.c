ce7ed689e421b6365251cc62efbd26e1223eb531
Simeon <61100437+simeon2002@users.noreply.github.com>
2025-04-18 23:07:08 +0200

    Add integration of motion sensor for servo control

--- GPIO_Examples/main/Servo_Control.c
+++ GPIO_Examples/main/Servo_Control.c
@@ -5,12 +5,14 @@
 #include "esp_log.h"
 #include "freertos/freertos.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
 #include "driver/gpio.h"
 
 #ifndef DEBUG
 #define DEBUG 1
 #endif
 
+#define PIN_MOTION_SENSOR GPIO_NUM_8
 #define PIN_BTN_SERVO GPIO_NUM_9
 #define PIN_SERVO GPIO_NUM_10
 
@@ -23,12 +25,14 @@
 #define SERVO_LEDC_CHANNEL LEDC_CHANNEL_0
 #define SERVO_LEDC_INTERRUPT LEDC_INTR_DISABLE
 
-static const char *TAG = "Servo motor";
-static TaskHandle_t servo_button_task_handle = NULL;
+#define BIT_SERVO_BTN_PRESSED (1 << 0)
+#define BIT_MOTION_SENSOR_CLEARED (1 << 1)
 
-/**
- * conversion of angle to duty cycle value based on given duty cycle resolution.
- */
+static const char *TAG = "Servo motor";
+EventGroupHandle_t servo_event_group;
+TaskHandle_t servo_button_task_handle = NULL;
+volatile bool servo_busy = false; // prevents repeated button ISR triggers
+
 int angle_to_duty(float angle)
 {
     float slope = (SERVO_MAX_DUTY_CYCLE - SERVO_MIN_DUTY_CYCLE) / 180.0f;
@@ -39,41 +43,59 @@ int angle_to_duty(float angle)
     return duty;
 }
 
-/**
- * servo button pressed causing task to start. note: only possible to press button once during one directional movement. (subsequent interrupts won't be resigstered.)
- */
 void IRAM_ATTR servo_button_isr_handler(void *args)
 {
-    vTaskResume(servo_button_task_handle);
+    if (!servo_busy) {
+        xEventGroupSetBitsFromISR(servo_event_group, BIT_SERVO_BTN_PRESSED, NULL);
+        ESP_EARLY_LOGI("ISR", "Button ISR triggered!\n");
+    }
 }
 
-/**
- * Task to run when button to move servo is pressed.
- */
+void IRAM_ATTR motion_sensor_isr_handler(void *args)
+{
+    xEventGroupSetBitsFromISR(servo_event_group, BIT_MOTION_SENSOR_CLEARED, NULL);
+    ESP_EARLY_LOGI("ISR", "Motion sensor ISR triggered!\n");
+}
+
 void servo_button_task(void *args)
 {
     int duty;
     for (;;)
     {
-        vTaskSuspend(servo_button_task_handle);
+        ESP_LOGI(TAG, "Waiting for door to open");
+        xEventGroupWaitBits(servo_event_group, BIT_SERVO_BTN_PRESSED, pdTRUE, pdFALSE, portMAX_DELAY);
+        servo_busy = true;
+
         duty = angle_to_duty(180.0f);
         ESP_ERROR_CHECK(ledc_set_fade_with_time(SERVO_LEDC_MODE, SERVO_LEDC_CHANNEL, duty, 750));
         ESP_ERROR_CHECK(ledc_fade_start(SERVO_LEDC_MODE, SERVO_LEDC_CHANNEL, LEDC_FADE_WAIT_DONE));
 
-        vTaskDelay(pdMS_TO_TICKS(5000));
+        // required delay of 5 ms.
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
+        ESP_LOGI(TAG, "Waiting for door to close");
+        xEventGroupWaitBits(servo_event_group, BIT_MOTION_SENSOR_CLEARED, pdTRUE, pdFALSE, portMAX_DELAY);
 
         duty = angle_to_duty(0.0f);
         ESP_ERROR_CHECK(ledc_set_fade_with_time(SERVO_LEDC_MODE, SERVO_LEDC_CHANNEL, duty, 750));
         ESP_ERROR_CHECK(ledc_fade_start(SERVO_LEDC_MODE, SERVO_LEDC_CHANNEL, LEDC_FADE_WAIT_DONE));
+
+        // clear interrupts after servo movement finsihed to remove accidental interrupts.
+        xEventGroupClearBits(servo_event_group, BIT_SERVO_BTN_PRESSED);
+        servo_busy = false;
     }
 }
 
-/**
- * Setup of servo motor
- */
+void motion_sensor_init()
+{
+    gpio_reset_pin(PIN_MOTION_SENSOR);
+    gpio_set_direction(PIN_MOTION_SENSOR, GPIO_MODE_INPUT);
+    gpio_set_intr_type(PIN_MOTION_SENSOR, GPIO_INTR_NEGEDGE);
+    gpio_isr_handler_add(PIN_MOTION_SENSOR, motion_sensor_isr_handler, NULL);
+}
+
 void servo_init()
 {
-    // timer configuration
     ledc_timer_config_t servo_timer_config = {
         .speed_mode = SERVO_LEDC_MODE,
         .duty_resolution = SERVO_LEDC_DUTY_RES,
@@ -82,7 +104,6 @@ void servo_init()
         .clk_cfg = LEDC_AUTO_CLK};
     ESP_ERROR_CHECK(ledc_timer_config(&servo_timer_config));
 
-    // channel configuration
     ledc_channel_config_t servo_channel_config = {
         .gpio_num = PIN_SERVO,
         .speed_mode = SERVO_LEDC_MODE,
@@ -93,33 +114,26 @@ void servo_init()
         .hpoint = 0};
     ESP_ERROR_CHECK(ledc_channel_config(&servo_channel_config));
 
-    // install fade function
     ledc_fade_func_install(0);
 }
 
-/**
- * Set up configuration for the servo button with ISR included.
- */
 void servo_button_init()
 {
     gpio_reset_pin(PIN_BTN_SERVO);
     gpio_set_direction(PIN_BTN_SERVO, GPIO_MODE_INPUT);
     gpio_set_intr_type(PIN_BTN_SERVO, GPIO_INTR_NEGEDGE);
     gpio_set_pull_mode(PIN_BTN_SERVO, GPIO_PULLUP_ONLY);
-    gpio_isr_handler_add(PIN_BTN_SERVO, servo_button_isr_handler, NULL); // set up isr for the servo button.
+    gpio_isr_handler_add(PIN_BTN_SERVO, servo_button_isr_handler, NULL);
 }
 
 void app_main(void)
 {
-    // enable per-gpio interrupt service engine
+    servo_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1));
-
-    // initialize servo motor.
     servo_init();
-    // init servo button
     servo_button_init();
+    motion_sensor_init();
 
-    // tasks
     xTaskCreate(servo_button_task,
                 "servo_button_task",
                 2048,
